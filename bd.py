1c5b85d66b056ff45341357e5cc5c27bd3025aff
lfrr_81 <lfrr81@gmail.com>
2022-09-21 09:49:27 -0500

    aptfile

--- bd.py
+++ bd.py
@@ -3,7 +3,7 @@ import pyodbc
 
 def dbconnection():
     try:
-        return pyodbc.connect("Driver={FreeTDS};"
+        return pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
             "Server=ia2.database.windows.net;"
             "Database=IA2;"
             "UID=Greenia2;"
